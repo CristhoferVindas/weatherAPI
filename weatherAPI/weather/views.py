0488de214cd1a9343a13e20706a631dd919a56d3
Cristhofer Eli Vindas Muñoz <120971218+CristhoferVindas@users.noreply.github.com>
2025-04-06 23:35:46 -0600

    FEAT: Add current weather endpoint with caching for city queries

--- weatherAPI/weather/views.py
+++ weatherAPI/weather/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+import requests
+from django.core.cache import cache
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import WeatherSerializer
+from django.conf import settings
 
 # Create your views here.
+class WeatherAPIView(APIView):
+    def get(self, request, city_name):
+        cached_data = cache.get(city_name)
+        if cached_data:
+            return Response(cached_data)
+
+        api_key = settings.OPENWEATHERMAP_API_KEY
+        url = f'http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units=metric'
+
+        response = requests.get(url)
+        data = response.json()
+
+        if data.get("cod") != 200:
+            return Response({"error": "Ciudad no encontrada"}, status=404)
+
+        weather_data = {
+            "city": data["name"],
+            "temperature": data["main"]["temp"],
+            "description": data["weather"][0]["description"],
+            "humidity": data["main"]["humidity"],
+            "pressure": data["main"]["pressure"]
+        }
+
+        cache.set(city_name, weather_data, timeout=600)
+
+        serializer = WeatherSerializer(weather_data)
+        return Response(serializer.data)
+
