8a2d16d23a9f57da055eb9ef17ddd985dde7a661
Cristhofer Eli Vindas Muñoz <120971218+CristhoferVindas@users.noreply.github.com>
2025-04-06 23:45:10 -0600

    FEAT: Add WeatherQuery model to store weather query history

--- weatherAPI/weather/models.py
+++ weatherAPI/weather/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class WeatherQuery(models.Model):
+    city = models.CharField(max_length=100)
+    temperature = models.FloatField()
+    description = models.CharField(max_length=255)
+    queried_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.city} - {self.temperature}°C"
