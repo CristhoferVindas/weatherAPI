43f4ec2374c6aeabdf8f00a010ba4713b8c1794c
Cristhofer Eli Vindas Muñoz <120971218+CristhoferVindas@users.noreply.github.com>
2025-04-06 23:47:56 -0600

    FEAT: Add WeatherSerializer for serializing weather data responses

---
+++ weatherAPI/weather/serializers.py
@@ -0,0 +1,9 @@
+# clima/serializers.py
+from rest_framework import serializers
+
+class WeatherSerializer(serializers.Serializer):
+    city = serializers.CharField(max_length=100)
+    temperature = serializers.FloatField()
+    description = serializers.CharField(max_length=100)
+    humidity = serializers.IntegerField()
+    pressure = serializers.IntegerField()
